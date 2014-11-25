50c1685830de09257c8f9cb63322cfdddb00d896
Erispoe <favrebulle@gmail.com>
2014-11-25 14:39:06 -0800

    Basic price predictor

--- server.R
+++ server.R
@@ -1,6 +1,33 @@
 library(shiny)
 library(ggplot2)
+dataset <- diamonds
 
 shinyServer(function(input, output) {
+  observe({
+
+    f <- formula(paste("price", "~",paste(input$variable, collapse = " + ")))
+    model = lm(f, data = dataset)
+    
+    n <- data.frame(carat = NA, 
+                    cut = NA, 
+                    color = NA)
+    
+    if('carat' %in% input$variable){n$carat <- input$carat}
+    if('cut' %in% input$variable){n$cut <- input$cut}
+    if('color' %in% input$variable){n$color <- input$color}
+    
+    p <- predict(model, newdata=n, interval="predict")
+    
+    output$fit <- renderUI(HTML(
+      paste("Your diamond is expected to sell for <strong>$",round(p[1]), 
+            "</strong> and has a 95% chance of selling between $", round(p[2]), "and $", round(p[3]), 
+            collapse = "")
+      )
+    )
+    
+    output$plot <- 
+    
+  })
   
+
 })
--- ui.R
+++ ui.R
@@ -6,31 +6,62 @@ dataset <- diamonds
 shinyUI(fluidPage(
   titlePanel("Diamond Pricer"),
   "An app giving you the expected price of a diamond.",
-  "",
+  br(),br(),
   
   sidebarLayout(
     sidebarPanel(
       
-      checkboxGroupInput("variable", "What do you know about the diamond? Select at least one.",
+      checkboxGroupInput("variable", "What do you know about the diamond? Select at least one:",
                          c("Carat" = "carat",
                            "Cut" = "cut",
-                           "Color" = "color",
-                           "Clarity" = "clarity",
-                           "Total depth percentage" = "depth",
-                           "Table" = "table",
-                           "Length in mm" = "x",
-                           "Width in mm" = "y",
-                           "Depth in mm" = "z")),
+                           "Color" = "color"#,
+                           #"Clarity" = "clarity",
+                           #"Total depth percentage" = "depth",
+                           #"Table" = "table",
+                           #"Length in mm" = "x",
+                           #"Width in mm" = "y",
+                           #"Depth in mm" = "z"
+                           ),
+                         selected = "Carat"),
       
-      sliderInput("carat",
-                  "Carat:",
-                  min = 0,
-                  max = 6,
-                  value = 3)
+      conditionalPanel(
+        condition = "input.variable.indexOf('carat') > -1",
+        sliderInput("carat",
+                    "Carat:",
+                    min = 0,
+                    max = 6,
+                    value = 3)
+        ),
+      
+      conditionalPanel(
+        condition = "input.variable.indexOf('cut') > -1",
+        selectInput("cut",
+                    "Cut:",
+                    c("Fair" = "Fair",
+                      "Good" = "Good",
+                      "Very Good" = "Very Good",
+                      "Premium" = "Premium",
+                      "Ideal" = "Ideal"))
       ),
+      
+      conditionalPanel(
+        condition = "input.variable.indexOf('color') > -1",
+        selectInput("color",
+                    "Color (from D, best color to J, worst color):",
+                    c("D" = "D",
+                      "E" = "E",
+                      "F" = "F",
+                      "G" = "G",
+                      "H" = "H",
+                      "I" = "I",
+                      "J" = "J"))
+      )
+      
+    ),
     
     mainPanel(
-      "Main panel"
+      uiOutput("fit"),
+      
       )
     
     )
